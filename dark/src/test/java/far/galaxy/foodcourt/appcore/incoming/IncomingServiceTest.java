b57675922ec188440712e0fd30f9bc5dee4ddc60
Andrey Ganyushkin <aganyushki@wiley.com>
2019-03-05 16:30:41 +0300

    Fixed junit5 test run

--- dark/src/test/java/far/galaxy/foodcourt/appcore/incoming/IncomingServiceTest.java
+++ dark/src/test/java/far/galaxy/foodcourt/appcore/incoming/IncomingServiceTest.java
@@ -1,17 +1,17 @@
 package far.galaxy.foodcourt.appcore.incoming;
 
 import far.galaxy.foodcourt.entity.transaction.IncomingRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class IncomingServiceTest {
     @Mock
     private IncomingRepository incomingRepository;
